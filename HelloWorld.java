972312029b0ce400aabb70bd9eb655018b05d930
Luiz Paulo R. dos Santos <luizp36@yahoo.com>
2019-02-20 11:24:59 -0300

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+Classe HelloWorld{ //Create the main class HelloWorld.
+	public static void main(String[] args){ //Create the main method of the class.
+		System.out.println("Hello, world! I am a simple code in java.") //Print on the screen a massage.
+	}  //End of the main method block.
+}  //End of the class block.
