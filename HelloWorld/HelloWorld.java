b855cb6835e8b1e3f9106cc40e4f0bce6c887ffe
LuizProton <luizp36@yahoo.com>
2019-02-20 12:38:28 -0300

    Corrections needed to continue developing in this repository.

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ * FILE: HelloWorld.java
+ * AUTHOR: Proton
+ * DESCRIPTIONS: Print a massage 'Hello, world!' on the screen.
+*/
+
+//Create the main class HelloWorld.
+public class HelloWorld{ 
+	//Create the main method of the class.
+	public static void main(String[] args){ 
+		//Print on the screen a massage.
+		System.out.println("Hello, world! I am a simple code in java."); 
+	}  //End of the main method block.
+}  //End of the class block.
