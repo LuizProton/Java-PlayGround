d6392ac8c87480ba14759901973c13ad7dac1243
Luiz Paulo R. dos Santos <luizp36@yahoo.com>
2019-02-23 21:15:12 -0300

    Revisão dos comentários 01

--- EstruturaDeRepeticao01/src/Main.java
+++ EstruturaDeRepeticao01/src/Main.java
@@ -1,5 +1,5 @@
 /*
- * Este codigo contem uma estrutura de repetiçao que irá contar
+ * Este codigo contem uma estrutura de repetição que irá contar
  * de 0 a 10.
  */
  
@@ -14,13 +14,14 @@ public class Main{
 		
 		int count = 0; //Declarando e iniciando a variável count.
 		
-		//Bloco while que fará o loop de contagem.
+		//Bloco while que fará o loop da contagem.
 		while(count < 10){
+			//Mensagem que será exibida durante a contagem.
 			System.out.printf("%s Contando...\n", count);
 			count++;
 		}
 		
-		//Mensagem para finalizar o programa.
+		//Mensagem que finaliza o programa.
 		System.out.println("Fim da contage!");
 	}
 }
